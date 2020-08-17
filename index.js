c66d65bd5e51f8c3a58561a5337550a5f295e413
chavierto <hola@xavierlujan.com>
2020-08-17 12:14:51 -0400

    Initial back end setup
        -Clone repo
        -Branch to development
        -Install express, mongoose, nodemon, cors
        -Set up index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Set up express, controller, app
+const express = require('express');
+const taskController = require('./controllers/tasks');
+const app = express();
+const cors = require('cors');
+
+// Instantiate up cors
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/tasks', taskController);
+
+app.listen(8080, () => console.log('Tern is running on 8080!'));
