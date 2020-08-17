e53842ba21bb0a6a583e90181757edb4d9d726c2
mahi <mahlet.luluu@gmail.com>
2020-08-17 12:58:18 -0400

    Adding Schema and controller

---
+++ db/models/Task.js
@@ -0,0 +1,14 @@
+const mongoose = require('../db/connection');
+
+const TaskSchema = mongoose.Schema({
+	title: String,
+    body: String,
+    dueDate:Date,
+    createdDate:Date,
+    priority:Number,
+
+});
+
+const Task = mongoose.model('task', TaskSchema);
+
+module.exports = Task;
