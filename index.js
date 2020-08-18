240068d8ef93791962105f0d21f23864ab1b7043
chavierto <hola@xavierlujan.com>
2020-08-18 13:22:00 -0400

    Deployment prep
        -Add env. variable for connection.js
        -Add start: node index.js on package.json

--- index.js
+++ index.js
@@ -11,4 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/tasks', taskController);
 
-app.listen(8080, () => console.log('Tern is running on 8080!'));
+// Original port
+// app.listen(8080, () => console.log('Tern is running on 8080!'));
+
+// Modified port for Heroku/Atlas
+app.set('port', process.env.PORT || 8000);
+
+app.listen(app.get('port'), () => {
+	console.log(`✅ PORT: ${app.get('port')} 🌟`);
+});
