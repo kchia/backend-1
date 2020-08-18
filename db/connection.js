098b3fb16e9106e28c439f2f14cd624cf9ae9593
chavierto <hola@xavierlujan.com>
2020-08-18 13:03:52 -0400

    Update MongoURI to add process.env.NODE_ENV variable

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,29 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
+// set the uri for connecting to our local mongodb
+// const mongoURI = "mongodb://localhost/book-e";
+const mongoURI =
+	process.env.NODE_ENV === 'production'
+		? process.env.DB_URL
+		: 'mongodb://localhost/tasks';
+
+// connect to the database, with the imported mongoose instance
 mongoose
-	.connect('mongodb://localhost/tasks', { useNewUrlParser: true })
-	.then((conn) => {
-		console.log(`connected to mongodb on ${conn.connections[0].name} db`);
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+	.connect(mongoURI, { useNewUrlParser: true })
+	.then((instance) =>
+		console.log(`Connected to db: ${instance.connections[0].name}`)
+	)
+	.catch((error) => console.log('Connection failed!', error));
+
+//This is the original one we used:
+// mongoose
+// 	.connect('mongodb://localhost/tasks', { useNewUrlParser: true })
+// 	.then((conn) => {
+// 		console.log(`connected to mongodb on ${conn.connections[0].name} db`);
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
 
 module.exports = mongoose;
