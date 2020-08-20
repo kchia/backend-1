9592f3f0a62d5e6ab7478c068e2ab6c403e315ee
mahi <mahlet.luluu@gmail.com>
2020-08-20 15:09:57 -0400

    Adding a redirect route

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.get("/",(req,res)=>{
+	res.redirect("/tasks/");
+});
 app.use('/tasks', taskController);
 
 // Original port
